58795539683c502808072ff07ad26eadd7b12c5e
YaKashubo <kashubo.work@mail.ru>
2021-12-19 14:33:41 +0300

    2021.12.19-Poiseuille-NN

--- 2021.30.11-NeuralNetworks/main.py
+++ 2021.30.11-NeuralNetworks/main.py
@@ -8,17 +8,20 @@ import numpy as np
 from tensorflow import keras
 from tensorflow.keras.layers import Dense
 
-X = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
-Y = np.array([5, 5.2, 5.4, 5.6, 5.8, 6, 6.2, 6.4, 6.6, 6.8, 7])
+X = np.array([0, 1])
+Y = np.array([5, 7])
 
 model = keras.Sequential()
 model.add(Dense(units=1, input_shape=(1,), activation='linear'))
-model.compile(optimizer=keras.optimizers.Adam(0.1), loss='mean_squared_error')
+model.compile(optimizer=keras.optimizers.Adam(1), loss='mean_squared_error')
 
 model.fit(X, Y, epochs=1000, verbose=0)
 
-print(model.predict([-100]))  # должна вернуть f(10)=25
+print(model.predict([10]))  # должна вернуть f(10)=25
 print(model.get_weights())
 
 #   -1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1,
 #   3, 3.2, 3.4, 3.6, 3.8, 4, 4.2, 4.4, 4.6, 4.8,
+
+# , 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+#    5.2, 5.4, 5.6, 5.8, 6, 6.2, 6.4, 6.6, 6.8,
